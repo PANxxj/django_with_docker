4f6f2fd2d19a8bfb30112a05c8720a0634bfe100
Pankaj <Pankaj>
2025-11-08 22:00:18 +0530

    testing : testing the ci/cd

--- books/views.py
+++ books/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse  # <-- import HttpResponse
 
 def hello_world(request):
-    return HttpResponse('Hello world updated')  
+    return HttpResponse('Hello world Testing')  
